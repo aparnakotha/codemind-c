ffc312c6c02a7e67558617dbbb955d4f4c9c5b6e
aparnakotha <102216222+aparnakotha@users.noreply.github.com>
2022-10-30 10:41:49 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,r;     int s;     int rem=1;     int rem1=0;     cin>>n;     while (n!=0){         r=n%10;         rem*=r;         rem1+=r;         n=n/10;     }     if (rem==rem1){         cout<<"Spy Number";     }     else cout<<"Not Spy Number"; }

---
+++ Spy_Number.c
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,r;
+    int s;
+    int rem=1;
+    int rem1=0;
+    cin>>n;
+    while (n!=0){
+        r=n%10;
+        rem*=r;
+        rem1+=r;
+        n=n/10;
+    }
+    if (rem==rem1){
+        cout<<"Spy Number";
+    }
+    else cout<<"Not Spy Number";
+}
