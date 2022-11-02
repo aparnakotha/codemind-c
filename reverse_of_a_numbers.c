d9fd05add143477c7d11f6f7b8f8caa68a740ef0
aparnakotha <102216222+aparnakotha@users.noreply.github.com>
2022-11-02 10:30:36 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,r;     cin>>n;     int rem=0;     while (n!=0){         r=n%10;         n=n/10;         rem=rem*10+r;     }     cout<<rem; }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,r;
+    cin>>n;
+    int rem=0;
+    while (n!=0){
+        r=n%10;
+        n=n/10;
+        rem=rem*10+r;
+    }
+    cout<<rem;
+}
